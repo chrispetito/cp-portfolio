7b61c8160c78ef5cf937629c93f2f1c0e47e31bd
chrispetito <christopherpetito@gmail.com>
2020-01-06 15:58:11 -0800

    rearrange teck stack to match resume

--- src/data/techstack.js
+++ src/data/techstack.js
@@ -1,19 +1,19 @@
 const techstack = [
+  { name: "JavaScript", iconClass: "fab fa-js-square" },
   { name: "HTML5", iconClass: "fab fa-html5" },
   { name: "CSS3", iconClass: "fab fa-css3-alt" },
-  { name: "JavaScript", iconClass: "fab fa-js-square" },
   { name: "React", iconClass: "fab fa-react" },
-  { name: "Node.js", iconClass: "fab fa-node-js" },
-  { name: "Express.js", iconClass: "fas fa-code" },
-  { name: "REST API", iconClass: "fas fa-code" },
-  { name: "Reacstrap", iconClass: "fab fa-react" },
-  { name: "Material UI", iconClass: "fas fa-code" },
-  { name: "Jest", iconClass: "fas fa-code" },
-  { name: "SQLite", iconClass: "fas fa-database" },
+  { name: "Redux", iconClass: "fas fa-code" },
+  { name: "Node", iconClass: "fab fa-node-js" },
+  { name: "Express", iconClass: "fas fa-code" },
+  { name: "SQLite3", iconClass: "fas fa-database" },
   { name: "PostgreSQL", iconClass: "fas fa-database" },
-  { name: "Netlify", iconClass: "fas fa-server" },
-  { name: "Heroku", iconClass: "fas fa-server" },
-
+  { name: "Knex", iconClass: "fas fa-code" },
+  { name: "Material UI", iconClass: "fas fa-code" },
+  { name: "Reacstrap", iconClass: "fab fa-react" },
+  { name: "Python", iconClass: "fab fa-python" },
+  { name: "LESS", iconClass: "fas fa-code" },
+  { name: "SASS", iconClass: "fas fa-code" },
 ];
 
 export default techstack;
